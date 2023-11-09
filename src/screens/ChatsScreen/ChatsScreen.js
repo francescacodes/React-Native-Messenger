dff4a17572f770f60bc9c92d03492160dc102ef7
Francesca <73359090+francescabarnes@users.noreply.github.com>
2023-11-09 16:59:38 +0100

    end of day

--- src/screens/ChatsScreen/ChatsScreen.js
+++ src/screens/ChatsScreen/ChatsScreen.js
@@ -1,14 +1,26 @@
 import { FlatList, StyleSheet } from "react-native";
-import chats from "../../../assets/data/chats.json";
 import ChatListItem from "../../components/ChatListItem";
 import { API, graphqlOperation, Auth } from "aws-amplify";
-import { createChatRoom, createChatRoomUser } from "../../graphql/mutations";
-import { listChatRooms } from "../../graphql/queries";
+import { listChatRooms } from "./queries";
+import { useEffect, useState } from "react";
 
 const ChatsScreen = () => {
+  const [chatRooms, setChatRooms] = useState([]);
+  useEffect(() => {
+    const fetchChatRooms = async () => {
+      const authUser = await Auth.currentAuthenticatedUser();
+
+      const response = await API.graphql(
+        graphqlOperation(listChatRooms, { id: Auth.user.attributes.sub })
+      );
+
+      setChatRooms(response.data.getUser.ChatRoom.items);
+    };
+    fetchChatRooms();
+  }, []);
   return (
     <FlatList
-      data={chats}
+      data={chatRooms}
       renderItem={({ item }) => <ChatListItem chat={item} />}
       style={{ backgroundColor: "white" }}
     />
